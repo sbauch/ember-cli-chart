076ac85c5a3631c3e8f24a1680f5ff8723b972e5
sbauch <sbauch@gmail.com>
2015-10-28 12:21:23 -0400

    support for my donut charts

--- addon/components/ember-chart.js
+++ addon/components/ember-chart.js
@@ -11,10 +11,12 @@ export default Ember.Component.extend({
     var collection = this.get('collection');
     var dataAttr = this.get('metric');
     var labelAttr = this.get('label')
-    var data = this.dataForChart(collection, dataAttr)
-    this.set('data', data);
     var type = Ember.String.classify(this.get('type'));
     var options = Ember.merge({}, this.get('options'));
+    var data = this.dataForChart(collection, dataAttr)
+
+    this.set('data', data);
+
 
     var chart = new Chart(context)[type](data, options);
 
@@ -23,6 +25,7 @@ export default Ember.Component.extend({
       this.$().wrap("<div class='chart-parent'></div>");
       this.$().parent().append(legend);
     }
+
 
     this.set('chart', chart);
     this.addObserver('data', this, this.updateChart);
@@ -45,8 +48,7 @@ export default Ember.Component.extend({
     var collection = this.get('collection');
     var dataAttr = this.get('attribute');
     var labelAttr = this.get('label')
-
-    var data = this.get('collection').mapBy(dataAttr);
+    var data = this.dataForChart(collection, dataAttr)
 
     var redraw = ChartDataUpdater.create({
       data: data,
@@ -68,17 +70,24 @@ export default Ember.Component.extend({
   },
 
   dataForChart: function(collection, attr){
-    var arr = collection.toArray().reverse().mapBy(attr)
-    var data = {datasets: [{
-                      fillColor: 'rgba(255,255,255,.3)',
-                      strokeColor: '#fff',
-                      pointStrokeColor: '#fff',
-                      data: arr
-                    }],
-                labels: collection.toArray().reverse().map(function(item, index, enumerable){
-                  return moment(item.get('metricDate')).format("MMM Do");
-                })
-                }
+    if (this.get('type') == ('Doughnut' || 'Pie')){
+      var data = collection
+
+    }
+    else{
+      var arr = collection.toArray().reverse().mapBy(attr)
+
+      var data = {datasets: [{
+                        fillColor: 'rgba(255,255,255,.3)',
+                        strokeColor: '#fff',
+                        pointStrokeColor: '#fff',
+                        data: arr
+                      }],
+                  labels: collection.toArray().reverse().map(function(item, index, enumerable){
+                    return moment(item.get('metricDate')).format("MMM Do");
+                  })
+                  }
+    }
     return data
   }
 });
